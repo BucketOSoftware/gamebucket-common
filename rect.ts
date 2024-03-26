d2da8c0f1fca7ae7e0814bbe6bb433e92fc63a9a
Alex Michaud <alex@bucketosoftware.com>
2024-03-26 15:13:19 -0700

    (rect) Add some more bounds-oriented functions

--- rect.ts
+++ rect.ts
@@ -32,6 +32,47 @@ export const intersects = (a: Readonly<Rect>, b: Readonly<Rect>) =>
         b.origin.y + b.size.height - 1 < a.origin.y
     )
 
+/** Determine which side of a rect the point lies on, as a vector  */
+export function side(
+    r: Readonly<Rect>,
+    p: Readonly<GVec2>,
+    out: GVec2 = { x: 0, y: 0 },
+) {
+    if (p.x < r.origin.x) {
+        out.x = -1
+    } else if (p.x >= r.origin.x + r.size.width) {
+        out.x = 1
+    }
+
+    if (p.y < r.origin.y) {
+        out.y = -1
+    } else if (p.y >= r.origin.y + r.size.height) {
+        out.y = 1
+    }
+
+    return out
+}
+
+/** If `p` is outside of `r`, wrap it around to the other side. Mutates `p` */
+export function wrap(r: Readonly<Rect>, p: GVec2) {
+    p.x -= r.origin.x
+    p.y -= r.origin.y
+
+    // Most of the time the point will probably be less than width/height outside
+    // the rect, but we can't be sure of that, can we
+    let oldX
+    let oldY
+    do {
+        oldX = p.x
+        oldY = p.y
+        p.x = (p.x + r.size.width) % r.size.width
+        p.y = (p.y + r.size.height) % r.size.height
+    } while (oldX !== p.x || oldY !== p.y)
+
+    p.x += r.origin.x
+    p.y += r.origin.y
+}
+
 /** Clamp the given point to fit within the rect */
 export function clamp(r: Readonly<Rect>, p: GVec2) {
     p.x = scalarClamp(p.x, r.origin.x, r.origin.x + r.size.width - 1)
