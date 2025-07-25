9b20aeca04b1c8ad1eb756a88d10e96f89c5f38a
Alex Michaud <alex@bucketosoftware.com>
2025-07-24 17:01:00 -0700

    fix[math]: Avoid pulling in unnecessary library code

--- packages/math/format.ts
+++ packages/math/format.ts
@@ -4,7 +4,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-import { Quaternion, Euler } from 'three'
+import type { Quaternion, Euler } from 'three'
 
 import * as round from './round.js'
 import type { SVec2, SVec3, SQuat } from './geometry.js'
